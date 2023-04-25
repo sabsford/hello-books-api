09c490971084ce98b073250022f44552c952aac4
SF <Sabriaford23@gmail.com>
2023-04-24 23:13:32 -0700

    raise exceptions and helper funtion

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -15,6 +15,19 @@ books = [
 hello_world_bp = Blueprint("hello_world", __name__)
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} not found"}, 400))
+    
+    for book in books:
+        if book.id == book_id:
+            return book
+        
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+
 @books_bp.route("", methods=["GET"])
 def handle_books():
     books_response = []
@@ -28,18 +41,12 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        return {"message":f"book {book_id} not found"}, 404
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-    return {"message":f"book {book_id} not found"}, 404
+    book = validate_book(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
